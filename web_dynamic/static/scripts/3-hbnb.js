66cf22a5ddc0038a1c1334593ae20d84b5a4bc50
Rd-Feng <394@holbertonschool.com>
2019-02-22 14:26:06 -0800

    merging

---
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -0,0 +1,42 @@
+let url = 'http://0.0.0.0:5001/api/v1/status/';
+let places_search = 'http://0.0.0.0:5001/api/v1/places_search/'
+$('document').ready(function () {
+  const amenityChecked = {};
+  const amenityCheckboxArray = Array.from($('div.amenities input:checkbox'));
+  for (let i = 0; i < amenityCheckboxArray.length; i++) {
+    amenityCheckboxArray[i].addEventListener('change', () => {
+      if ($(amenityCheckboxArray[i]).is(':checked')) {
+        amenityChecked[$(amenityCheckboxArray[i]).attr('data-id')] = $(amenityCheckboxArray[i]).attr('data-name');
+      } else {
+        delete amenityChecked[$(amenityCheckboxArray[i]).attr('data-id')];
+      }
+      // update h4 amenities list (ordered)
+      let list = [];
+      for (let id in amenityChecked) {
+        list.push(amenityChecked[id]);
+      }
+      list.sort();
+      if (list.length !== 0) {
+        $('div.amenities h4').html(list.join(', '));
+      } else {
+        $('div.amenities h4').html('&nbsp;');
+      }
+    });
+  }
+  $.get(url, function (data, statusText, xhr) {
+    if (statusText === 'success') {
+      $('DIV#api_status').toggleClass('available');
+    } else {
+      $('DIV#api_status').toggleClass('available');
+    }
+  });
+  $.ajax({
+    url: places_search,
+    type: 'POST',
+    data: '{}',
+    contentType: "application/json",
+    success: function (data) {
+      console.log(data);
+    }
+  });
+});
