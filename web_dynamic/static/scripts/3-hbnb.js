c9e0834b42022fecaff3c4efead451bf4d086396
Rd-Feng <394@holbertonschool.com>
2019-02-22 15:15:56 -0800

    init commit

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -33,10 +33,44 @@ $('document').ready(function () {
   $.ajax({
     url: placesSearch,
     type: 'POST',
-    data: '{}',
+    data: JSON.stringify({}),
     contentType: "application/json",
     success: function (data) {
-      console.log(data);
+      data.sort((first, second) => {
+        return first.name < second.name ? -1 : 1;
+      });
+      for (let place of data) {
+        $('.places').append(`
+          <article>
+      	    <div class="title">
+      	      <h2>` + place.name + `</h2>
+      	      <div class="price_by_night">
+      		` + place.price_by_night +
+      	  `   </div>
+      	    </div>
+      	    <div class="information">
+      	      <div class="max_guest">
+      		<i class="fa fa-users fa-3x" aria-hidden="true"></i>
+      		<br />
+      		` + place.max_guest + `Guests
+      	      </div>
+      	      <div class="number_rooms">
+      		<i class="fa fa-bed fa-3x" aria-hidden="true"></i>
+      		<br />
+      		` + place.number_rooms + `Bedrooms
+      	      </div>
+      	      <div class="number_bathrooms">
+      		<i class="fa fa-bath fa-3x" aria-hidden="true"></i>
+      		<br />
+      		` + place.number_bathrooms + `Bathroom
+      	      </div>
+      	    </div>
+      	    <div class="description">
+      	      ` + place.description +
+      	  ` </div>
+      	  </article>
+        `)
+      }
     }
   });
 });
