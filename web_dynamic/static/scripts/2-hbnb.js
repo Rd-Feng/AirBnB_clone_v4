9497fdd0555ba46f17c6f91dd68a1b0ec08d81e3
Rd-Feng <394@holbertonschool.com>
2019-03-05 07:37:41 -0800

    remove portnum

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,4 +1,4 @@
-let url = 'http://34.73.207.106:8002/api/v1/status/';
+let url = 'http://34.73.207.106/api/v1/status/';
 $('document').ready(function () {
   const amenityChecked = {};
   const amenityCheckboxArray = Array.from($('div.amenities input:checkbox'));
